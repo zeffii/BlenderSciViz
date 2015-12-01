b3add9deaed77d84fc7bb26de7a93d094536055a
Dealga McArdle <zeffii@users.noreply.github.com>
2015-12-01 23:43:46 +0100

    Update node_connection_grammar.py

--- node_connection_grammar.py
+++ node_connection_grammar.py
@@ -9,7 +9,7 @@ mat:        is a reference to a material
 
 link_str:   'node_name_origin|output_socket > node_name_destination|input_socket'
             easier to think of this as
-            'origin > destination' each side must include a node name and a socket name or index
+            'origin > destination' each side must include a node name and a socket name or index.
             
             if a node has several sockets with the same type (Color, or Vector for example)
             you must specify the socket using an index instead.
